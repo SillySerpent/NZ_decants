5937ac51da0774f59e45296d20ba589a35dab663
SillySerpent <josephayleshaa@gmail.com>
2025-01-19 01:50:22 +1300

    HTML and CSS

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+app.debug = True
+
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("main_menu.html")
+
 
 if __name__ == '__main__':
     app.run()
